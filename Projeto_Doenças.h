b581cd5b1cdb9ae347e65a7b1750b357a5125bdd
Jhonny <jhonnykva@gmail.com>
2021-03-29 13:02:16 -0300

    1.02

--- Projeto_Doenças.h
+++ Projeto_Doenças.h
@@ -2,16 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 
+//------------------------------ Árvore Doenças
 #define T 10
 
 typedef struct node{
-    int n;  //número de chaves
+    int n;  //número da chave
     int folha; //bool
     int chaves[2*T-1];  //limite superior
     struct node *filhos[2*T];
 
 }Arvore_Doencas;
 
-Arvore_Doencas Busca(Arvore_Doencas *a, int chave);
-Arvore_Doencas *Inserir(Arvore_Doencas *a, int chave);
+Arvore_Doencas Busca_Doenca(Arvore_Doencas *a, int chave);
+Arvore_Doencas *Inserir_Doenca(Arvore_Doencas *a, int chave);
+void Remover_Doença(Arvore_Doencas *a, int chave);
+void Balacear_Arvore_Doenca(Arvore_Doencas *a);
+
+//----------------------------- Tabela Hash Sintomas
+
+typedef struct node{
+    int k; //chave chave pesquisa
+    struct node *next;
+}No_THash;
+
+typedef struct hash{
+    int N_Entradas;
+    int N_Chaves_Armazenadas
+}
+
 
