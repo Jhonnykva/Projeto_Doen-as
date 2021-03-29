1f74683e9a37934897344e3bead3c7e383ba96ae
Jhonnykva <jhonnykva@gmail.com>
2021-03-29 13:31:42 -0300

    Update Projeto_Doenças.h

--- Projeto_Doenças.h
+++ Projeto_Doenças.h
@@ -16,18 +16,24 @@ typedef struct node{
 Arvore_Doencas Busca_Doenca(Arvore_Doencas *a, int chave);
 Arvore_Doencas *Inserir_Doenca(Arvore_Doencas *a, int chave);
 void Remover_Doença(Arvore_Doencas *a, int chave);
-void Balacear_Arvore_Doenca(Arvore_Doencas *a);
+Arvore_Doencas *Balacear_Arvore_Doenca(Arvore_Doencas *a);
 
 //----------------------------- Tabela Hash Sintomas
 
-typedef struct node{
+typedef struct node2{
     int k; //chave chave pesquisa
     struct node *next;
 }No_THash;
 
 typedef struct hash{
     int N_Entradas;
-    int N_Chaves_Armazenadas
-}
+    int N_Chaves_Armazenadas;
+    No_THash **list;
+}Hash;
+
+Hash *create_hash(int N_Entradas);
+void insert_chained(Hash *H, int k);
+
+//---------------------------- Associação da Tabela Hash com Árvore de doenças
 
 
