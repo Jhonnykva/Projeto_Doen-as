e37377b2522c751d8b9d5c04b6bdbf5f4d24174f
Jhonny <jhonnykva@gmail.com>
2021-03-29 11:46:16 -0300

    1.0

---
+++ Doenças.c
@@ -0,0 +1,23 @@
+#include "Doenças.h"
+
+Arvore_Doencas Busca(Arvore *a, int chave){//busca alguma chave
+    int i=0;
+    while((i<a->n)&&(k>a->chave[i])){
+        i++;
+    }
+    if((i<a->n)&&(k==a->chave[i])){
+        printf("Encontrado");
+    }
+    else if(a->folha){
+        printf("Nao-Encontrado");
+    }
+    else{
+        return Busca(a->filhos[i], k);
+    }
+}
+
+Arvore_Doencas *Inserir(Arvore_Doencas *a, int chave)
+
+int main(){
+
+}
