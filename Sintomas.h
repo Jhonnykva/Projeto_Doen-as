99860c56a0f9fbb8b17f5efeae47729d6e981226
Jhonnykva <jhonnykva@gmail.com>
2021-03-30 17:03:01 -0300

    Sintomas
    implementação para criar, inserir e distribuir nós na Hash para string.

--- Sintomas.h
+++ Sintomas.h
@@ -1,16 +1,17 @@
 //----------------------------- Tabela Hash Sintomas
 typedef struct sintomas_node
 {
-  int k; //chave chave pesquisa
-  struct node *next;
-} sintomas_node;
+  char sintoma[50]; //chave pesquisa
+  struct sintomas_node *next;
+} Sintomas_node;
 
 typedef struct hash
 {
-  int N_Entradas;
-  int N_Chaves_Armazenadas;
-  sintomas_node **list;
+  int M;//entradas
+  int N;//chaves
+  Sintomas_node **list;
 } Hash;
 
-Hash *create_hash(int N_Entradas);
-void inserir(Hash *H, int k);
+Hash *create_hash(int M);
+void inserir(Hash *H,const char *sintoma);
+int funcaoHash(const char *sintoma,int M);
